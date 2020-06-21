3c8e14a8b3e89ce5106c0413ff61e95188ed399e
dorsafjaballah <jaballahdorsaf9@gmail.com>
2020-06-21 11:13:11 +0200

    second commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,10 +13,9 @@ const CardDesc = () => {
   <Card>
     <Card.Img variant="top" src={impact1} />
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
+      <Card.Title>the impact of ifresni on the amount of waste </Card.Title>
       <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
+      A single Tunisian's daily waste is about 0.815 kg per day ; our aim is to reduce it by 20 % reaching at least 0.5 kg per day.
       </Card.Text>
     </Card.Body>
     <Card.Footer>
@@ -26,24 +25,26 @@ const CardDesc = () => {
   <Card>
     <Card.Img variant="top" src={impact3} />
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
+      <Card.Title> waste gategories</Card.Title>
       <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
+      waste gategories : since our job is to help you seperate daily waste , let's have a look first at its  distribution :
+Plastic : takes the biggest part by 42.3 %  and it is highly dangerous because it takes at least 450 years to decompose
+Organic: 23% of waste , it is not dangerous since it is easy to recycle and decomposes quickly
+Paper : 15.4% , 20 years to decompose and is made out of trees so we're losing trees as well
+Metal: 11.5% , not a threat because it is easily recycled
+Batteries: even though they are only 7.7% of our waste they are the most dangerous because they leak their toxins into landfills
       </Card.Text>
     </Card.Body>
     <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
+      <small className="text-muted"></small>
     </Card.Footer>
   </Card>
   <Card>
     <Card.Img variant="top" src={impact2} />
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
+      <Card.Title>waste rate</Card.Title>
       <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
+      waste rate: in 2014 waste reached up to 2.5 million tons , and these numbers are increasing significantly by 2.5 %
       </Card.Text>
     </Card.Body>
     <Card.Footer>
